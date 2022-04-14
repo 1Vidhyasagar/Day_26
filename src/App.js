8c993d254d62f9575bd64450cf58b90b0564f0fa
1Vidhyasagar <vidhyasagarmyana9598.com>
2022-04-15 01:59:49 +0530

    Day_26

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import {Card, CardContent, Typography, CardActions, Button, Grid} from '@mui/material';
 
-function App() {
+// import MenuIcon from '@mui/icons-material/Menu';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2} style={{margin:'auto', padding:'auto'}}>
+      <Grid item>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+         FREE
+        </Typography>
+        <Typography variant="h5" component="div">
+        </Typography>
+        <Typography variant="h5" sx={{ mb: 1.5 }} color="text.secondary">
+        <b>$0/month</b>
+        </Typography>
+        <Typography >
+        Single User
+          <br />
+          {'5GB Storage'}     <br />
+          {'Unlimited Public Projects'}          <br />
+          {'Community Access'}          <br />
+          {'Unlimited Private Projects'}          <br />
+          {'Dedicated Phone Support'}          <br />
+          {'Free Subdomain'}          <br />
+          {'Monthly Status Reports'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" variant="contained">Button</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+
+    <Grid item>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+         PLUS
+        </Typography>
+        <Typography variant="h5" component="div">
+        </Typography>
+        <Typography variant="h5" sx={{ mb: 1.5 }} color="text.secondary">
+        <b>$0/month</b>
+        </Typography>
+        <Typography variant="h7">
+        Single User
+          <br />
+          {'5GB Storage'}     <br />
+          {'Unlimited Public Projects'}          <br />
+          {'Community Access'}          <br />
+          {'Unlimited Private Projects'}          <br />
+          {'Dedicated Phone Support'}          <br />
+          {'Free Subdomain'}          <br />
+          {'Monthly Status Reports'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" variant="contained">Button</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+
+    <Grid item>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+         PRO
+        </Typography>
+        <Typography variant="h5" component="div">
+        </Typography>
+        <Typography variant="h5" sx={{ mb: 1.5 }} color="text.secondary">
+        <b>$0/month</b>
+        </Typography>
+        <Typography variant="h7">
+        Single User
+          <br />
+          {'5GB Storage'}     <br />
+          {'Unlimited Public Projects'}          <br />
+          {'Community Access'}          <br />
+          {'Unlimited Private Projects'}          <br />
+          {'Dedicated Phone Support'}          <br />
+          {'Free Subdomain'}          <br />
+          {'Monthly Status Reports'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" variant="contained">Button</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+    </Grid>
   );
 }
 
-export default App;
+
+
+
+
+
